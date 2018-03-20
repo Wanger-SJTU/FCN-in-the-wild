1b253aa4f8f222e074506ff638b53837c5fd737a
Howie Cheng <howiechen@sjtu.edu.cn>
2018-03-20 22:46:36 +0800

    add CAN

---
+++ FCN/model.py
@@ -0,0 +1,18 @@
+
+
+import torch
+import numpy as np
+import torchvision.models as models
+
+from torch.autograd import Variable
+from torchvision import datasets, models, transforms
+
+
+class CAN(torch.nn):
+
+	def __init__(self):
+		pass
+
+
+	def forward(self):
+		pass
